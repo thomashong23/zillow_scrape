1c4010076ac25795dd1895b5a2fccb3b4a312f7d
thomashong23 <th70@rice.edu>
2024-07-23 11:34:25 +0100

    moving away from spiders, parsing the js script for full page info

--- zillow.com/zillow/spiders/home_prices.py
+++ zillow.com/zillow/spiders/home_prices.py
@@ -36,40 +36,17 @@ class HomePricesSpider(scrapy.Spider):
         'DOWNLOAD_DELAY': 2,
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36'
     }
-
-    def __init__(self, *args, **kwargs):
-        super(HomePricesSpider, self).__init__(*args, **kwargs)
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-
     def parse(self, response):
-        self.driver.get(response.url)
+       
 
-        wait = WebDriverWait(self.driver, 10)
-        previous_length = 0
-
-        while True:
-            wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div.StyledPropertyCardDataWrapper')))
-            
-            # Scroll to the bottom to load more listings
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)  # Wait for more listings to load
-            
-            # Check the number of listings loaded
-            current_length = len(self.driver.find_elements(By.CSS_SELECTOR, 'div.StyledPropertyCardDataWrapper'))
-            
-            # If no new listings are loaded, break the loop
-            if current_length == previous_length:
-                break
-
-            previous_length = current_length
-
-        # Get the page source after scrolling
-        html = self.driver.page_source
-        self.driver.quit()
-
+      
         # Use Scrapy to parse the page source
-        response = HtmlResponse(url=response.url, body=html, encoding='utf-8')
+        
 
-        all_pages = response.css('div.StyledPropertyCardDataWrapper')
+        all_pages = response.xpath('//div[contains(@class, "StyledPropertyCardDataWrapper")]')
+        next_page = "https://www.zillow.com/" +response.xpath('//ul[contains(@class, "PaginationList")]/li[contains(@class, "PaginationJumpItem")]').css('::attr(href)')[1]
         for listing in all_pages:
             yield get_listing_info(listing)
+            
+        yield response.follow(next_page, callback=self.parse)
+        
